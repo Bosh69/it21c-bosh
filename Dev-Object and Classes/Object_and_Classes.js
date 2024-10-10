31d668622d595374b1e4488cadce9ee9d7328031
Bosh69 <bvshermusic@gmail.com>
2024-10-10 23:04:12 +0800

    Added get element by ID. To display on html

--- Dev-Object and Classes/Object_and_Classes.js
+++ Dev-Object and Classes/Object_and_Classes.js
@@ -12,3 +12,9 @@ console.log("Age :" + person1.age);
 console.log("Occupation :" + person1.occupation);
 console.log("Hobby :" + person1.hobby);
 console.log("Tagline; " + person1);
+
+document.getElementById("person1-name").innerHTML = "Name: " + person1.name;
+document.getElementById("person1-age").innerHTML = "Age: " + person1.age;
+document.getElementById("person1-occupation").innerHTML = "Occupation: " + person1.occupation;
+document.getElementById("person1-hobby").innerHTML = "Hobby: " + person1.hobby;
+document.getElementById("person1-tagline").innerHTML = "Tagline: " + person1.tagline;
